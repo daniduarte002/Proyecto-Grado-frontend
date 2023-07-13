ca1c2479802eb0c6e3769a7b4c453f064aed268f
daniduarte002 <danielduarte9029@gmail.com>
2023-07-13 17:38:05 -0500

    Subir todos los archivos

--- Index.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener("DOMContentLoaded", function() {
-    var images = document.getElementsByTagName("img");
-    for (var i = 0; i < images.length; i++) {
-      images[i].addEventListener("contextmenu", function(e) {
-        e.preventDefault();
-      });
-    }
-  });
-  
